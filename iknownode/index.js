b5ea27104e336bfa8ec64a54d97ad6afa2c5dac3
nikolasmelui <slak@samaradom.ru>
2017-07-15 13:11:28 +0400

    Add server.js file. Add exports and require - module structure. Add check for module.parent in required module.

--- iknownode/index.js
+++ iknownode/index.js
@@ -1,12 +1,2 @@
-const http = require('http');
-
-const hostname = 'localhost';
-const port = '3000';
-const server = http.createServer((req, res) => {
-  res.statusCode = '200';
-  res.setHeader('Content-Type', 'texft/plain');
-  res.end('Hello!');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+let server = require('./server');
+  server.run();
---
+++ iknownode/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = '3000';
+function run () {
+  const server = http.createServer((req, res) => {
+    res.statusCode = '200';
+    res.setHeader('Content-Type', 'texft/plain');
+    res.end('Server is running!');
+  });
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+// if(module.parent) {
+//   exports.run = run;
+// } else { run(); }
+
+exports.run = run;
