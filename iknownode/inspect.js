b4e4864dd88936db3516d842ee92ca989eae7d94
nikolasmelui <slak@samaradom.ru>
2017-07-18 19:33:22 +0400

    Add inspect.js

---
+++ iknownode/inspect.js
@@ -0,0 +1,18 @@
+let util = require('util');
+
+function Car(name) {
+  this.name = name;
+}
+
+function Mersedes(name) {
+  this.name = name;
+}
+
+util.inherits(Mersedes, Car);
+
+Car.prototype.beep = function () {
+  console.log('Beep!');
+};
+
+let mersedes = new Mersedes('mersedes');
+mersedes.beep();
