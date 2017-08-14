5944cfeb705b65a4cdbfa900e30ae4bf869521c7
nikolasmelui <slak@samaradom.ru>
2017-08-14 23:44:33 +0400

    Add comments how to start server with nodemon on CLI-port

--- iknownode/argv/server.js
+++ iknownode/argv/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
 var opts = (require('optimist').argv);
-
+// Чтобы запустить сервер на нужном нам порту из командной строки под нодемоном для динамической разработки там нужно применить следующие флаги
+// nodemon --inspect-brk -- server.js --port=3000
 http.createServer((req, res) => res.end('The server is running')).listen(opts.port);
