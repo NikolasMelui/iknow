9d283697d2b391c8788ee43d3cadf8a7cbfeb9c7
NikolasMelui <slak@samaradom.ru>
2018-04-11 18:34:30 +0400

    Find new cool dotenv module.

--- iknownode/https/server.js
+++ iknownode/https/server.js
@@ -5,4 +5,8 @@ https
 		res.writeHead(200);
 		res.end('Hello!');
 	})
-	.listen(3000);
+	.listen(3000, function() {
+});
+		console.log(process.env.HELLO);
+		require('dotenv').config();
+		console.log(process.env.HELLO);
